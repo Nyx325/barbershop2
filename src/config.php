213139507b19fdaf04a92006a7d01ecd68136ab4
Nyx325 <omar.roman.2828@gmail.com>
2025-03-09 22:05:41 -0600

    minor changes

--- src/config.php
+++ src/config.php
@@ -4,7 +4,6 @@ namespace App;
 require __DIR__ . "/../vendor/autoload.php";
 
 use Dotenv\Dotenv;
-use Illuminate\Database\Capsule\Manager as Capsule;
 
 // Cargar variables de entorno
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
